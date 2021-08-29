20234525be3b988b50ab087c81b3e5f501966365
Matheus Paiva <matheus.a.paiva@gmail.com>
2021-08-29 11:33:53 -0300

    refactor(tree): remove node string method

--- tree/node.go
+++ tree/node.go
@@ -1,7 +1,5 @@
 package tree
 
-import "fmt"
-
 // Node represents a binary tree node.
 type Node struct {
 	Data  int
@@ -12,18 +10,4 @@ type Node struct {
 // NewNode creates a new node.
 func NewNode(data int) *Node {
 	return &Node{Data: data}
-}
-
-func (n *Node) String() string {
-	var left, right int
-
-	if n.Left != nil {
-		left = n.Left.Data
-	}
-
-	if n.Right != nil {
-		right = n.Right.Data
-	}
-
-	return fmt.Sprintf("Data: %d, Left: %v, Right: %v", n.Data, left, right)
 }
