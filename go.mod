444a61745d5c09cc79a68af3412a6634b1b37e2e
Matheus Paiva <matheus.a.paiva@gmail.com>
2021-08-30 16:46:18 -0300

    chore: remove gore as a dep

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module algorithms
 
 go 1.15
-
-require github.com/x-motemen/gore v0.5.3 // indirect
--- trie/node.go
+++ trie/node.go
@@ -1,15 +1,13 @@
 package trie
 
-import "fmt"
-
 // AlphabetSize represents the quantity of letters in the alphabet.
 const AlphabetSize = 26
 
 // Node represents a trie node.
 type Node struct {
-	children        [AlphabetSize]*Node
-	isEnd           bool
-	wordsInChildren int
+	children   [AlphabetSize]*Node
+	isEnd      bool
+	wordsCount int
 }
 
 // Trie represents a trie.
@@ -17,22 +15,22 @@ type Trie struct {
 	root *Node
 }
 
-func (t *Trie) String() string {
-	return fmt.Sprintf("Root: %v", t.root)
-}
-
 // Add will take in a word and insert it into the trie.
 func (t *Trie) Add(word string) {
 	wordLen := len(word)
 	currentNode := t.root
+
 	for i := 0; i < wordLen; i++ {
 		charIndex := word[i] - 'a'
+
 		if currentNode.children[charIndex] == nil {
 			currentNode.children[charIndex] = &Node{}
 		}
+
 		currentNode = currentNode.children[charIndex]
-		currentNode.wordsInChildren++
+		currentNode.wordsCount++
 	}
+
 	currentNode.isEnd = true
 }
 
@@ -40,28 +38,35 @@ func (t *Trie) Add(word string) {
 func (t *Trie) Find(word string) bool {
 	wordLen := len(word)
 	currentNode := t.root
+
 	for i := 0; i < wordLen; i++ {
 		charIndex := word[i] - 'a'
+
 		if currentNode.children[charIndex] == nil {
 			return false
 		}
+
 		currentNode = currentNode.children[charIndex]
 	}
+
 	return currentNode.isEnd
 }
 
 func (t *Trie) FindCount(word string) int {
 	wordLen := len(word)
 	currentNode := t.root
+
 	for i := 0; i < wordLen; i++ {
 		charIndex := word[i] - 'a'
+
 		if currentNode.children[charIndex] == nil {
 			return 0
 		}
+
 		currentNode = currentNode.children[charIndex]
 	}
 
-	return currentNode.wordsInChildren
+	return currentNode.wordsCount
 }
 
 // New creates a new trie.
