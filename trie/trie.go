4cb117cce2423e276d2627798e3c7261bc7dc34a
Matheus Paiva <matheus.a.paiva@gmail.com>
2021-08-31 17:30:46 -0300

    feat(trie): implement AddRecursive method

--- trie/trie.go
+++ trie/trie.go
@@ -29,6 +29,26 @@ func (t *Trie) Add(word string) {
 	currentNode.isEnd = true
 }
 
+// AddRecursive will take in a word and insert it into the trie recursively.
+func (t *Trie) AddRecursive(word string) {
+	t.add(word, 0, t.root)
+}
+
+func (t *Trie) add(word string, index int, node *Node) {
+	if len(word) == index {
+		node.isEnd = true
+		return
+	}
+
+	charIndex := word[index] - 'a'
+	if node.children[charIndex] == nil {
+		node.children[charIndex] = &Node{}
+	}
+	node.children[charIndex].size++
+
+	t.add(word, index+1, node.children[charIndex])
+}
+
 // Find will take in a word and return true if that word is in the trie or false if it is not.
 func (t *Trie) Find(word string) bool {
 	wordLen := len(word)
