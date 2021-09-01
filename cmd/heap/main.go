c688fa355a9378860258eda06d25da1ceb870f5c
Matheus Paiva <matheus.a.paiva@gmail.com>
2021-09-01 19:09:37 -0300

    feat(heap): update main example

--- cmd/heap/main.go
+++ cmd/heap/main.go
@@ -6,13 +6,37 @@ import (
 )
 
 func main() {
-	m := heap.NewMaxHeap()
+	var medians []float64
+	lowers := heap.NewMinHeap()
+	highers := heap.NewMaxHeap()
 
-	// items := []int{3, 4, 8, 9, 7, 10, 9, 15, 20, 13}
 	items := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	for _, i := range items {
-		m.Add(i)
-		fmt.Println(m)
+	for _, number := range items {
+		lowers.Add(number)
+		highers.Add(number)
+
+		medians = append(medians, median(lowers, highers))
 	}
 
+	fmt.Println(lowers)
+	fmt.Println(highers)
+	fmt.Println(medians)
+}
+
+func median(lowers *heap.MinHeap, highers *heap.MaxHeap) float64 {
+	var biggerHeap, smallerHeap heap.Heap
+
+	if lowers.Size() > highers.Size() {
+		biggerHeap = lowers
+		smallerHeap = highers
+	} else {
+		biggerHeap = highers
+		smallerHeap = lowers
+	}
+
+	if biggerHeap.Size() == smallerHeap.Size() {
+		return (float64(biggerHeap.Peek()) + float64(smallerHeap.Peek())) / 2
+	}
+
+	return float64(biggerHeap.Peek())
 }
