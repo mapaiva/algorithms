18a144cab75bcebe7dfd0a61af128b274252aa18
Matheus Paiva <matheus.a.paiva@gmail.com>
2021-09-06 08:54:22 -0300

    refactor(cmd): refactor knapsack algorithm a bit

--- cmd/problems/knapsack/main.go
+++ cmd/problems/knapsack/main.go
@@ -7,16 +7,16 @@ import (
 func knapsack(items [][]int, capacity int) int {
 	n, w := len(items)+1, capacity+1
 	table := make([][]int, n)
-	for j := 0; j < n; j++ {
+	for j := 0; j < n; j++ { // O(n) onde n = len(items)+1 => O(n)
 		table[j] = make([]int, w)
 	}
 
-	for i := 1; i < n; i++ {
-		for j := 0; j < w; j++ {
-			if items[i-1][1] > j {
+	for i := 1; i < n; i++ { // O(n)
+		for j := 0; j < w; j++ { // O(w) onde w = len(capacity)+1 => O(w)
+			if weight(items, i-1) > j {
 				table[i][j] = table[i-1][j]
 			} else {
-				k := table[i-1][j-(items[i-1][1])] + items[i-1][0]
+				k := table[i-1][j-(weight(items, i-1))] + value(items, i-1)
 				m := table[i-1][j]
 
 				table[i][j] = max(k, m)
@@ -24,7 +24,17 @@ func knapsack(items [][]int, capacity int) int {
 		}
 	}
 
+	// O(n) + [ O(n) * O(w) ] = O(n) + O(n*w) = O(n+n*w) = O(n*w)
+
 	return table[n-1][w-1]
+}
+
+func weight(items [][]int, i int) int {
+	return items[i][1]
+}
+
+func value(items [][]int, i int) int {
+	return items[i][0]
 }
 
 func max(a, b int) int {
